37f1234852a5972f2793737ff5fa1d4b3f7f6b93
Quantum-Cucumber <quantumcucumber@protonmail.com>
2022-03-04 13:50:15 +1100

    Pronoun sharing via a link

--- index.js
+++ index.js
@@ -41,15 +41,29 @@ const PRESETS = {
     },
 }
 
+const PRONOUNFIELDS = ["subjective", "objective", "possessive", "possessivePlural", "reflexive"]
 
-function selectPreset(element) {
-    const preset = PRESETS[element.value];
+
+function selectPreset(presetString) {
+    /* Fills the form fields based on the preset string */
+
+    if (presetString === "custom") {
+        document.getElementById("custom").style.display = "block";
+    }
+    else {
+        document.getElementById("custom").style.display = "none";
+    }
+
+    if (!(presetString in PRESETS)) {
+        // TODO: Make the fields blank instead
+        return;
+    }
+
+    const preset = PRESETS[presetString];
     
-    document.getElementById("subjective").value = preset.subjective;
-    document.getElementById("objective").value = preset.objective;
-    document.getElementById("possessive").value = preset.possessive;
-    document.getElementById("possessivePlural").value = preset.possessivePlural;
-    document.getElementById("reflexive").value = preset.reflexive;
+    for (const field of PRONOUNFIELDS) {
+        document.getElementById(field).value = preset[field];
+    }
 
     if (preset.singular) {
         document.getElementById("singular").checked = true;
@@ -57,4 +71,71 @@ function selectPreset(element) {
     else {
         document.getElementById("plural").checked = true;
     }
+}
+
+function onLoad() {
+    /* Fill the pronoun fields from the url params */
+
+    // Get the url params
+    const params = new URLSearchParams(window.location.search);
+
+    // If a preset is specified, use that
+    if (params.has("preset")) {
+        const preset = params.get("preset");
+        document.getElementById("presets").value = preset;
+        selectPreset(preset);
+
+        return;
+    }
+
+    // Fill in the form fields based on the params
+    document.getElementById("presets").value = "custom";
+    document.getElementById("custom").style.display = "block";
+
+    for (const field of PRONOUNFIELDS) {
+        document.getElementById(field).value = params.get(field);
+    }
+
+    document.getElementById("singular").checked = params.get("singular") === "true";
+    document.getElementById("plural").checked = params.get("singular") === "false";
+}
+
+function getUrl() {
+    /* Builds the sharable url */
+
+    const baseUrl = window.location.href.split("?")[0];
+    const params = [];
+
+    // If a preset is selected, directly use that
+    const presetDropdown = document.getElementById("presets");
+    if (presetDropdown.value !== "select" && presetDropdown.value !== "custom") {
+        return baseUrl + "?preset=" + presetDropdown.value;
+    }
+
+    // Get each form field's value and add to the query params if a value is set
+    for (const field of PRONOUNFIELDS) {
+        const value = document.getElementById(field).value;
+        if (value) {
+            params.push(field + "=" + value);
+        }
+    }
+
+    // Determine if the pronouns are singular or plural and add to params
+    if (document.getElementById("singular").checked) {
+        params.push("singular=true");
+    }
+    else if (document.getElementById("plural").checked) {
+        params.push("singular=false");
+    }
+
+    return baseUrl + "?" + params.join("&");
+}
+
+function copyUrl() {
+    /* Copy the url to the clipboard and set the current url */
+
+    const url = getUrl();
+    navigator.clipboard.writeText(url);
+    window.history.pushState({}, "", url);
+    alert("Copied sharable url");
 }
